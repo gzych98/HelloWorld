c8aeef8af50bed3691ee924b4082864ec2eac156
gzych98 <111496194+gzych98@users.noreply.github.com>
2024-01-02 17:01:26 +0100

    qqqqq

--- src/components/MainFormPol2.js
+++ src/components/MainFormPol2.js
@@ -19,7 +19,7 @@ const MainFormPol2 = () => {
         });
     };
 
-    const sendTestEmail = async () => {
+    const sendTestEmailTemp = async () => {
         console.log("Wysyłam do serwera: ", data);
 
         const response = await fetch('../pages/api/sendEmail', {
@@ -36,6 +36,47 @@ const MainFormPol2 = () => {
         const responseData = await response.json();
         console.log(a)
         console.log("Odpowiedź serwera: ", responseData);
+    }
+
+    const sendTestEmail = async () => {
+        console.log("Wysyłam do serwera: ", data);
+
+        try {
+            const response = await fetch('../pages/api/sendEmail', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    subject: 'Formularz GT Code Lab',
+                    message: 'To jest treść wiadomości:',
+                    formData: data
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error(`Błąd HTTP: status ${response.status}`);
+            }
+
+            let responseData;
+            try {
+                responseData = await response.json();
+            } catch (e) {
+                throw new Error("Błąd przetwarzania odpowiedzi JSON.");
+            }
+
+            console.log("Odpowiedź serwera: ", responseData);
+            // Tutaj możesz obsłużyć dane odpowiedzi, np. wyświetlić powiadomienie
+        } catch (error) {
+            console.error("Wystąpił błąd: ", error.message);
+            // Tutaj możesz obsłużyć błąd, np. wyświetlić powiadomienie o błędzie
+            Swal.fire({
+                title: 'Błąd',
+                text: error.message,
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            });
+        }
     }
 
     const handleSubmit = () => {

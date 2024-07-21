14a4644b4eada3bf90783393727c66e422804932
gregorytomek1@gmail.com <111496194+gzych98@users.noreply.github.com>
2024-07-21 13:17:00 +0200

    poprawa linkow

--- src/app/poradniki/page.tsx
+++ src/app/poradniki/page.tsx
@@ -12,7 +12,7 @@ const articles = [
         title: 'Python dla inżyniera',
         date: '15 lipca 2024',
         excerpt: 'Podstawy programowania w Pythonie dla inżynierów.',
-        link: 'programowanie/podstawy-python',
+        link: 'poradniki/podstawy-python',
         image: 'python-basic.png'
     },
     {
@@ -20,7 +20,7 @@ const articles = [
         title: 'Wprowadzenie do ML',
         date: '20 lipca 2024',
         excerpt: 'Podstawy Machine Learning i jak zacząć.',
-        link: 'programowanie/ml-introduction',
+        link: 'poradniki/ml-introduction',
         image: 'article_ml_intro.png'
     },
     // Można dodać więcej artykułów w podobnym formacie

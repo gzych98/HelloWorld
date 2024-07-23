5cb35bf673cdd9fae0869f82a4ecef56e8818f71
gzych98 <111496194+gzych98@users.noreply.github.com>
2024-07-23 19:53:10 +0200

    coś go bolało

--- src/app/poradniki/image-to-chart/page.jsx
+++ src/app/poradniki/image-to-chart/page.jsx
@@ -7,6 +7,7 @@ import Footer_pol from '../../footer';
 import NavBar02 from '@/components/navbar/navbar';
 import './styles_article.css';
 import DownloadForm from '@/components/DownloadForm/DownloadForm';
+import Image from 'next/image';
 
 const content = [
     { id: 'app_description', title: 'Opis Aplikacji' },
@@ -31,13 +32,13 @@ export default function ArticlePage() {
             <main>
                 <NavBar02 />
                 <div className={`image-overlay ${isImageOpen ? 'open' : ''}`} onClick={handleOverlayClick}>
-                    <img src="/img-to-chart-1.png" alt="Article Cover" className="image-fullscreen" />
+                    <Image src="/img-to-chart-1.png" alt="Article Cover" layout="responsive" width={700} height={475} className="image-fullscreen" />
                 </div>
                 <div className="page" id="how-it-works">
                     <div className="content-container">
                         <div className="article">
                             <div className="article-cover-container" onClick={handleImageClick}>
-                                <img src="/img-to-chart-1.png" alt="Article Cover" className="article-cover" />
+                                <Image src="/img-to-chart-1.png" alt="Article Cover" layout="responsive" width={700} height={475} className="article-cover" />
                                 <div className="cover-text">Zrzut ekranu aplikacji i jej kod</div>
                             </div>
                             <header className="article-header">
@@ -48,22 +49,22 @@ export default function ArticlePage() {
                             <Element name="app_description" id="app_description">
                                 <h2>Opis Aplikacji</h2>
                                 <p>Ręczne przepisywanie danych z wykresów może być żmudne i czasochłonne. Dzięki Pythonowi można znacznie ułatwić sobie życie, automatyzując te zadania.</p>
-                                <p>Narzędzie "Interactive Plot Point Picker" to przykład, jak Python może pomóc inżynierom i analitykom w codziennej pracy. Umożliwia ono łatwe odczytywanie danych z wykresów: wczytujesz obraz, zaznaczasz punkty, przeliczasz je na rzeczywiste wartości i eksportujesz wyniki do pliku CSV.</p>
+                                <p>Narzędzie &quot;Interactive Plot Point Picker&quot; to przykład, jak Python może pomóc inżynierom i analitykom w codziennej pracy. Umożliwia ono łatwe odczytywanie danych z wykresów: wczytujesz obraz, zaznaczasz punkty, przeliczasz je na rzeczywiste wartości i eksportujesz wyniki do pliku CSV.</p>
                             </Element>
                             
                             <Element name="workflow" id="workflow">
                                 <h2>Jak działa aplikacja</h2>
                                 <p>Kroki do użycia aplikacji:</p>
                                 <ol>
-                                    <li>Załaduj obraz, używając przycisku "Load Image".</li>
+                                    <li>Załaduj obraz, używając przycisku &quot;Load Image&quot;.</li>
                                     <li>Kliknij na cztery punkty na obrazie, aby ustawić osie (X1, X2, Y1, Y2).</li>
                                     <li>Wprowadź rzeczywiste wartości dla tych punktów w odpowiednich polach.</li>
-                                    <li>Kliknij "Set Axes", aby zastosować te wartości.</li>
+                                    <li>Kliknij &quot;Set Axes&quot;, aby zastosować te wartości.</li>
                                     <li>Kliknij na dowolne inne punkty na obrazie, aby zebrać punkty danych.</li>
                                     <li>Punkty będą wyświetlane w tabeli z ich rzeczywistymi współrzędnymi.</li>
-                                    <li>Użyj "Plot Points", aby zobaczyć wybrane punkty na wykresie.</li>
-                                    <li>"Clear Points" usunie wszystkie wybrane punkty.</li>
-                                    <li>"Reset Axes" wyczyści punkty osi i wartości.</li>
+                                    <li>Użyj &quot;Plot Points&quot;, aby zobaczyć wybrane punkty na wykresie.</li>
+                                    <li>&quot;Clear Points&quot; usunie wszystkie wybrane punkty.</li>
+                                    <li>&quot;Reset Axes&quot; wyczyści punkty osi i wartości.</li>
                                 </ol>
                             </Element>
 

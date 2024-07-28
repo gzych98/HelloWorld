d282aecdd279232dc496f75cc7ad23c0fe57a4fd
gzych98 <111496194+gzych98@users.noreply.github.com>
2024-07-28 11:04:26 +0200

    prosty_timer

--- src/app/prosty-timer/page.tsx
+++ src/app/prosty-timer/page.tsx
@@ -1,39 +1,63 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import Footer_pol from '../footer';
-import NavBar_pol from '../navbar';
+import NavBar02 from '@/components/navbar/navbar';
 import './takeYourTime.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { faRankingStar, faPenToSquare, faChartLine, faCloudArrowUp, faScrewdriverWrench, faSliders } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import NavBar02 from '@/components/navbar/navbar';
+import { faClock, faLightbulb, faTasks, faRobot, faChartLine, faListCheck } from '@fortawesome/free-solid-svg-icons';
+
 
 
 
 export default function HowItWorks() {
-    const [hover1, setHover1] = useState(false);
-    const [hover2, setHover2] = useState(false);
-    const [hover3, setHover3] = useState(false);
-    const [hover4, setHover4] = useState(false);
-    const [hover5, setHover5] = useState(false);
     return (
         <main>
             <NavBar02 />
-            <div className="page first_page" id='take-your-time-hero'>
+            <section className="hero-section" id="take-your-time-hero">
                 <div className="content-container">
-                    <div className="Column-width-50 display-left" >
-                        <h2>Take yout time!</h2>
-                        <h1>Prosty Time</h1>
+                    <div className="text-column">
+                        <h2>Take your time!</h2>
+                        <h1>Prosty Timer</h1>
                         <p>Rozwijaj się każdego dnia! Aplikacja motywująca do osiągania celów, zdobywania umiejętności i nieustannego samodoskonalenia.</p>
-                        <a target="_blank" rel="noopener noreferrer" href='https://www.prostytimer.pl'>
-                            <button className='button-dark'>Pobierz</button>
+                        <ul className="features-list">
+                            <li><FontAwesomeIcon icon={faClock} className="fa-icon" /> Ustaw czas odliczania, nadaj etykietę i kliknij start. To wszystko!</li>
+                            <li><FontAwesomeIcon icon={faLightbulb} className="fa-icon" /> Brak zbędnych funkcji, tylko to, co naprawdę potrzebne.</li>
+                            <li><FontAwesomeIcon icon={faChartLine} className="fa-icon" /> Aplikacja zbiera dane o Twojej pracy, pomagając Ci lepiej zarządzać swoim czasem.</li>
+                            <li><FontAwesomeIcon icon={faTasks} className="fa-icon" /> Działa zarówno na Windows, jak i macOS.</li>
+                        </ul>
+                        <a target="_blank" rel="noopener noreferrer" href="https://prostytimer.pl/index.php/sklep/">
+                            <button className="button-dark">Pobierz</button>
                         </a>
                     </div>
-                    <div className="Column-width-50 image" id='how-it-works'>
+                    <div className="image-column">
+                        <a href="https://prostytimer.pl/index.php/sklep/" target="_blank">
+                            <img src="/app_1.png" alt="Prosty Timer app screenshot" className="image" />
+                        </a>
                     </div>
                 </div>
-            </div>
+            </section>
+
+            <section className="info-section" id="info-section">
+                <div className="content-container">
+                    <div className="full-width-column">
+                        <h1>Dlaczego warto zarządzać swoim czasem?</h1>
+                        <ul className="info-list">
+                            <li><FontAwesomeIcon icon={faClock} className="fa-icon" /> Dobre zarządzanie czasem pozwala na wykonanie więcej zadań w krótszym czasie, co zwiększa produktywność.</li>
+                            <li><FontAwesomeIcon icon={faLightbulb} className="fa-icon" />  Planowanie i organizacja pomagają uniknąć chaosu i niepotrzebnego stresu.</li>
+                            <li><FontAwesomeIcon icon={faTasks} className="fa-icon" /> Umożliwia skoncentrowanie się na priorytetach i długoterminowych celach, co prowadzi do większych osiągnięć.</li>
+                        </ul>
+                        <h1>Jak Prosty Timer może Ci pomóc?</h1>
+                        <ul className="info-list">
+                            <li><FontAwesomeIcon icon={faRobot} className="fa-icon" /> Automatyzuje mierzenie czasu pracy nad zadaniami, pozwalając skupić się na ich wykonaniu.</li>
+                            <li><FontAwesomeIcon icon={faChartLine} className="fa-icon" /> Zbiera dane o Twojej pracy, pomagając lepiej zarządzać czasem i wyciągać wnioski.</li>
+                            <li><FontAwesomeIcon icon={faListCheck} className="fa-icon" /> Brak zbędnych funkcji, tylko to, co naprawdę potrzebne, aby wspierać efektywność.</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
             <Footer_pol />
-        </main >
-    )
+        </main>
+    );
 }

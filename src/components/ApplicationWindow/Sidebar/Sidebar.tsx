70bb43d54cd6a7892efbf728d476dc76f6856de7
gzych98 <111496194+gzych98@users.noreply.github.com>
2024-08-31 15:51:58 +0200

    update sidebar

--- src/components/ApplicationWindow/Sidebar/Sidebar.tsx
+++ src/components/ApplicationWindow/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { auth } from '@/fireBase/firebaseConfig';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import { FaTasks, FaChartBar, FaInfoCircle, FaSignOutAlt, FaSignInAlt, FaUser, FaCog } from 'react-icons/fa';
 import './Sidebar.css';
 
 const Sidebar: React.FC = () => {
@@ -13,20 +14,36 @@ const Sidebar: React.FC = () => {
 
     return (
         <div className="custom-sidebar">
-            <h2>Sidebar</h2>
+            <div className="sidebar-header">
+                <h2>AppName</h2>
+            </div>
             <div className="custom-sidebar-links">
-                <Link to="/task-list">Lista Zadań</Link>
-                <Link to="/statistics">Statystyki</Link>
-                <Link to="/info">Informacje</Link>
+                <Link to="/task-list" className="sidebar-link">
+                    <FaTasks className="sidebar-icon" /> Lista Zadań
+                </Link>
+                <Link to="/statistics" className="sidebar-link">
+                    <FaChartBar className="sidebar-icon" /> Statystyki
+                </Link>
+                <Link to="/info" className="sidebar-link">
+                    <FaInfoCircle className="sidebar-icon" /> Informacje
+                </Link>
+                <Link to="/settings" className="sidebar-link">
+                    <FaCog className="sidebar-icon" /> Ustawienia
+                </Link>
             </div>
             <div className="user-panel">
                 {user ? (
-                    <div>
+                    <div className="user-info">
+                        <FaUser className="user-icon" />
                         <span>{user.displayName || user.email}</span>
-                        <button onClick={handleLogout}>Logout</button>
+                        <button onClick={handleLogout} className="logout-button">
+                            <FaSignOutAlt className="button-icon" /> Logout
+                        </button>
                     </div>
                 ) : (
-                    <Link to="/login">Login</Link>
+                    <Link to="/login" className="login-link">
+                        <FaSignInAlt className="button-icon" /> Login
+                    </Link>
                 )}
             </div>
         </div>
